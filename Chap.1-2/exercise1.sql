de0fbc4a4df4f37fc496652199546c58ec1a1d8a
Wenhui <62164172+WENHUIn@users.noreply.github.com>
2020-03-22 19:22:24 -0400

    Delete exercise1.sql

--- Chap.1-2/exercise1.sql
+++
@@ -1,7 +0,0 @@
--- Exercise regexp --
-select *
-from customers
--- where first_name regexp 'elka|ambur'
--- where last_name regexp 'ey$|on'
--- where last_name regexp '^my|se' 
--- where last_name regexp 'B[RU]'
